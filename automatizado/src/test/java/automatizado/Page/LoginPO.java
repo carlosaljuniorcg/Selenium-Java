cd20b46129b4ed428530dc0dd2c067b8a4a25282
Carlos Antônio <carlosaljuniorcg@hotmail.com>
2022-07-12 16:25:57 -0300

    Update 12072022

--- automatizado/src/test/java/automatizado/Page/LoginPO.java
+++ automatizado/src/test/java/automatizado/Page/LoginPO.java
@@ -1,5 +1,6 @@
 package automatizado.Page;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -26,11 +27,29 @@ public class LoginPO extends BasePO {
      */
     public LoginPO(WebDriver driver) {
         super(driver);  
-        
+    }
+
+    public void escrever(WebElement input, String texto) {
+        input.clear();
+        input.sendKeys(texto + Keys.TAB);
     }
 
     public String obterMensagem() {
         return spanMensagem.getText();
     }
     
+    /**
+     * Método que tenta executar a ação de logar no sistema
+     * @param email E-mail para tentativa de login
+     * @param senha Senha para tentativa de login
+     */
+    public void executarAcaoDeLogar(String email, String senha) {
+        escrever(inputEmail, email);
+        escrever(inputSenha, senha);
+        btnEntrar.click(); 
+    }
+
+    public String obterTituloPagina() {
+        return driver.getTitle();
+    }
 }
--- automatizado/src/test/java/automatizado/test/LoginTest.java
+++ automatizado/src/test/java/automatizado/test/LoginTest.java
@@ -3,30 +3,98 @@ package automatizado.test;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import automatizado.Page.LoginPO;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class LoginTest extends BaseTest {
     
     private static LoginPO loginPage;
+
+    //private void executarAcaoDeLogar(String email, String senha) {
+    //    loginPage.escrever(loginPage.inputEmail, email);
+    //    loginPage.escrever(loginPage.inputSenha, senha);
+    //    
+    //    loginPage.btnEntrar.click(); 
+    //}
+
 
     @BeforeClass    
     public static void prepararTestes() {
         loginPage = new LoginPO(driver);
     }
+
     @Test
     public void TC001_naoDeveLogarNoSistemaComEmailESenhaEmBranco() {
         
-        loginPage.inputEmail.sendKeys(" ");
-        loginPage.inputSenha.sendKeys(" ");
-
-        loginPage.btnEntrar.click();
+        loginPage.executarAcaoDeLogar("", "");
         String mensagem = loginPage.obterMensagem();
         
         assertEquals(mensagem, "Informe usuário e senha, os campos não podem ser brancos.");
-        
-               
+    
+    }
 
+    @Test
+    public void TC002_naoDeveLogarNoSistemaComEmailIncorretoESenhaEmBranco() {
+        
+        loginPage.executarAcaoDeLogar("teste", "");
+        String mensagem = loginPage.obterMensagem();
+        
+        assertEquals(mensagem, "Informe usuário e senha, os campos não podem ser brancos.");
+      
+    }
+
+    @Test
+    public void TC003_naoDeveLogarNoSistemaComEmailEmBrancoESenhaIncorreta() {
+        
+        loginPage.executarAcaoDeLogar("", "teste");
+        String mensagem = loginPage.obterMensagem();
+        
+        assertEquals(mensagem, "Informe usuário e senha, os campos não podem ser brancos.");
+      
+    }
+    
+    @Test
+    public void TC004_naoDeveLogarNoSistemaComEmailESenhaIncorretos() {
+        
+        loginPage.executarAcaoDeLogar("teste", "teste");
+        String mensagem = loginPage.obterMensagem();
+        
+        assertEquals(mensagem, "E-mail ou senha inválidos");
+      
+    }
+
+    @Test
+    public void TC005_naoDeveLogarNoSistemaComEmailCorretoESenhaIncorreto() {
+        
+        loginPage.executarAcaoDeLogar("admin@admin.com", "teste");
+        String mensagem = loginPage.obterMensagem();
+        
+        assertEquals(mensagem, "E-mail ou senha inválidos");
+      
+    }
+
+    @Test
+    public void TC006_naoDeveLogarNoSistemaComEmailIncorretoESenhaCorreta() {
+        
+        loginPage.executarAcaoDeLogar("Aadmin@admin.com", "admin@123");
+        String mensagem = loginPage.obterMensagem();
+        
+        assertEquals(mensagem, "E-mail ou senha inválidos");
+      
+    }
+
+    @Test
+    public void TC007_DeveLogarNoSistemaComEmailESenhaCorretos() {
+        
+        loginPage.executarAcaoDeLogar("admin@admin.com", "admin@123");
+        //String mensagem = loginPage.obterMensagem();
+        
+        assertEquals(loginPage.obterTituloPagina(), "Controle de Produtos");
+      
     }
 }
+ 
